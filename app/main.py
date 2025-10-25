d7f358f4ff76f08943d1b37e8d058a9dc0763ec9
root <root@disgusting-spring.aeza.network>
2025-10-25 14:21:53 +0200

    monitoring

--- app/main.py
+++ app/main.py
@@ -62,4 +62,4 @@ def convert():
 
 if __name__ == '__main__':
     print("Starting Flask app...")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
